a3ea2271cb9b3b1dd4970bdd24b0bcf0e83c91bd
mjalbert13 <mjalbert13@gmail.com>
2019-08-15 15:45:11 -0400

    updated APPr js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
+import axios from 'axios'
+import {Link, Route} from 'react-router-dom'
 import logo from "./logo.svg";
 import Signup from './Components/Sign-up';
+import Loggin from './Components/logIn-form'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super() 
+    this.state ={
+      loggedIn: false,
+      name: null
+    }
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser =this.updateUser.bind(this) 
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser(userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log("getting user")
+      console.log(response.data)
+      if(response.data.user) {
+        console.log("there is a user session saved")
+        this.setState({
+          loggedIn: true,
+          name: response.data.user.name
+        })
+      } else {
+        console.log("no user yet")
+        this.setState({
+          loggedIn: false,
+          name: null
+        })
+      }
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Signup />
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        {this.state.loggedIn && <p>Join the party, {this.state.name}</p>}
+        <Route exact path="/" component={Home} />
+        <Route path="/login" render={() => <Loggin updateUser={this.updateUser}/>}/>
+        <Route path="/signup" render ={() => <Signup/>}/>
       </div>
     );
   }
